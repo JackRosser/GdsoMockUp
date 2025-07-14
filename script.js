ebd2b5f9d02f84901e1637e2ed872f67725d3b9e
Giacomo Rossettini <g.rossettini@axxelera.it>
2025-07-14 10:13:02 +0200

    Ordine in corso

--- script.js
+++ script.js
@@ -2,19 +2,26 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const openBtn = document.getElementById("openMenu");
-  const closeBtn = document.getElementById("closeMenu");
   const offcanvas = document.querySelector(".custom-offcanvas");
   const backdrop = document.querySelector(".custom-offcanvas-backdrop");
+  const goToOrder = document.querySelector("#goToOrder");
+  const menuItems = document.querySelectorAll(".menuItem");
 
   openBtn.addEventListener("click", () => {
     offcanvas.classList.add("open");
   });
 
-  closeBtn.addEventListener("click", () => {
+  backdrop.addEventListener("click", () => {
     offcanvas.classList.remove("open");
   });
 
-  backdrop.addEventListener("click", () => {
+  goToOrder.addEventListener("click", () => {
     offcanvas.classList.remove("open");
+  });
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      offcanvas.classList.remove("open");
+    });
   });
 });
